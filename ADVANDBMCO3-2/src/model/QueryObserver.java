a832a8cd64f8a312f472d8b2f8047947edde3956
Shayane Tan <shayane_tan@yahoo.com>
2016-04-06 09:19:34 +0800

    deleted unnecessary files

--- ADVANDBMCO3-2/src/model/QueryObserver.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.sql.ResultSet;
-
-import com.sun.rowset.CachedRowSetImpl;
-
-public interface QueryObserver extends Observer {
-	public void updateResultSet(CachedRowSetImpl rs);
-}
